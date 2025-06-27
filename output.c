77441823ac938f6d27976b76507c2dc17ecbd913
Danielle Soares <silvasoaresdanielle2@gmail.com>
2025-06-27 20:00:19 -0300

    feat: operações aritméticas (teste)

---
+++ output.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    int x = 6;
+    int y = 5;
+    int z = 2;
+    int a = 1;
+    int b = 0;
+    int c = 10;
+    x = (x + y);
+    z = (y - z);
+    int l = (x / y);
+    int k = (10 * x);
+    int j = (x * x);
+    int m = ((1 + y) * (c - 4));
+    return 0;
+}
---
+++ test/exemplos/operations.rb
@@ -0,0 +1,13 @@
+x = 6
+y = 5
+z = 2
+a = true
+b = false
+c = 10
+
+x = x + y
+z = y - z
+l = x / y
+k = 10*x
+j = x*x
+m = (1 + y) * (c - 4)
