0a1227b06f44fe231df29a0066423df8eb1a6f1b
gbevi <gbevi281@gmail.com>
2025-06-27 20:58:02 -0300

    fix: tes 1 e 3

--- output.c
+++ output.c
@@ -2,11 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-void soma(int a, int b) {
-    return (a + b);
-}
-
 
 int main(void) {
+    int x = 0;
+    int y = 0;
+    if ((x < 5)) {
+        int y = 10;
+    }
+    if ((x == 1)) {
+        int y = 1;
+    }
+    else {
+        int y = 2;
+    }
+    if ((x == 1)) {
+        int y = 1;
+    }
+    else if ((x <= 2)) {
+        int y = 2;
+    }
+    else if ((x >= 6)) {
+        int y = 4;
+    }
+    else if ((x > 3)) {
+        int y = 8;
+    }
+    else if (x) {
+        int y = 3;
+    }
+    else if ((x == 1)) {
+        int y = (y + x);
+    }
+    else if ((x == 3)) {
+        int y = (y - x);
+    }
     return 0;
 }
